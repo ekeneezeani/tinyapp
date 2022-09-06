44feadd24214b35318e3140f7ef2cefc0dbb3521
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-09-06 15:12:05 -0500

    implementation of the redirect to the actual longURL

--- express_server.js
+++ express_server.js
@@ -1,50 +1,59 @@
 const express = require("express");
+const { generateRandomString } = require("./generateRandomString");
 
 const app = express();
 const PORT = 8080;
 
-app.set("view engine","ejs");
-
-
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res) =>{
+app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const id = generateRandomString();
+  const { longURL } = req.body;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+  // console.log(req.body);
+  // res.send("Ok");
 });
 
-app.get('/urls.json',(req,res)=>{
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/urls/new',(req,res)=>{
-  res.render('urls_new');
+app.get('/u/:id', (req,res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id]
+  res.redirect(longURL)
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const id  = req.params.id;
-  const templateVars = { id, longURL: urlDatabase[id]};
+  const id = req.params.id;
+  const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
 });
 
-app.get('/hello', (req,res) => {
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get('/urls',(req,res)=>{
-  const templateVars = { urls: urlDatabase};
-  res.render("urls_index",templateVars);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
