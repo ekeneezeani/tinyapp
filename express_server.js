54056538831061b84293bda93899ae98207a5a82
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-09-06 14:11:57 -0500

    implemented the middleware and the app.post

--- express_server.js
+++ express_server.js
@@ -5,17 +5,29 @@ const PORT = 8080;
 
 app.set("view engine","ejs");
 
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res) =>{
   res.send("Hello!");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get('/urls.json',(req,res)=>{
   res.json(urlDatabase);
+});
+
+app.get('/urls/new',(req,res)=>{
+  res.render('urls_new');
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -32,7 +44,6 @@ app.get('/urls',(req,res)=>{
   const templateVars = { urls: urlDatabase};
   res.render("urls_index",templateVars);
 });
-
 
 app.listen(PORT, ()=>{
   console.log(`Example app listening on port ${PORT}!`);
